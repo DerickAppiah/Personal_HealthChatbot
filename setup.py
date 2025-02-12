89854424e0198e01ba9ab771500a58093cc1f506
DerickAppiah <appiahdherrick@gmail.com>
2025-02-12 15:57:12 -0500

    creating embedding model

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Personal_HealthChatbot Project',
+    version= '0.0.0',
+    author= 'Derick Appiah, Gizem',
+    author_email= 'appiahdherrick@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
